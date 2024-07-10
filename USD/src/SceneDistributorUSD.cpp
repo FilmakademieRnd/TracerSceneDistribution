3cdc07fa9ad7fc64fd23b0816b55f00ceb2e69ae
Simon Spielmann <sspielma@filmakademie.de>
2024-07-10 11:20:55 +0200

    - update header

--- USD/src/SceneDistributorUSD.cpp
+++ USD/src/SceneDistributorUSD.cpp
@@ -1,35 +1,35 @@
 /*
 -----------------------------------------------------------------------------
-This source file is part of VPET - Virtual Production Editing Tool
-http://vpet.research.animationsinstitut.de/
-http://github.com/FilmakademieRnd/VPET
-
-Copyright (c) 2023 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
-
-This project has been initiated in the scope of the EU funded project
-Dreamspace under grant agreement no 610005 in the years 2014, 2015 and 2016.
-http://dreamspaceproject.eu/
-Post Dreamspace the project has been further developed on behalf of the
-research and development activities of Animationsinstitut.
-
-The VPET components Scene Distribution and Synchronization Server are intended
-for research and development purposes only. Commercial use of any kind is not
-permitted.
-
-There is no support by Filmakademie. Since the Scene Distribution and
-Synchronization Server are available for free, Filmakademie shall only be
-liable for intent and gross negligence; warranty is limited to malice. Scene
-Distribution and Synchronization Server may under no circumstances be used for
-racist, sexual or any illegal purposes. In all non-commercial productions,
-scientific publications, prototypical non-commercial software tools, etc.
-using the Scene Distribution and/or Synchronization Server Filmakademie has
-to be named as follows: ÒVPET-Virtual Production Editing Tool by Filmakademie
-Baden-WŸrttemberg, Animationsinstitut (http://research.animationsinstitut.de)Ò.
-
-In case a company or individual would like to use the Scene Distribution and/or
-Synchronization Server in a commercial surrounding or for commercial purposes,
-software based on these components or any part thereof, the company/individual
-will have to contact Filmakademie (research<at>filmakademie.de).
+TRACER Scene Distribution Plugin USD
+ 
+Copyright (c) 2024 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Labs
+https://research.animationsinstitut.de/tracer
+https://github.com/FilmakademieRnd/TracerSceneDistribution
+ 
+TRACER Scene Distribution Plugin USD is a development by Filmakademie
+Baden-Wuerttemberg, Animationsinstitut R&D Labs in the scope of the EU funded
+project MAX-R (101070072) and funding on the own behalf of Filmakademie
+Baden-Wuerttemberg.  Former EU projects Dreamspace (610005) and SAUCE (780470)
+have inspired the TRACER Scene Distribution Plugin USD development.
+ 
+The TRACER Scene Distribution Plugin USD is intended for research and
+development purposes only. Commercial use of any kind is not permitted.
+ 
+There is no support by Filmakademie. Since the TRACER Scene Distribution Plugin
+USD is available for free, Filmakademie shall only be liable for intent
+and gross negligence; warranty is limited to malice. TRACER Scene Distribution
+Plugin USD may under no circumstances be used for racist, sexual or any
+illegal purposes. In all non-commercial productions, scientific publications,
+prototypical non-commercial software tools, etc. using the TRACER Scene
+Distribution Plugin USD Filmakademie has to be named as follows: 
+"TRACER Scene Distribution Plugin USD by Filmakademie
+Baden-Württemberg, Animationsinstitut (http://research.animationsinstitut.de)".
+ 
+In case a company or individual would like to use the TRACER Scene Distribution
+Plugin USD in a commercial surrounding or for commercial purposes,
+software based on these components or  any part thereof, the company/individual
+will have to contact Filmakademie (research<at>filmakademie.de) for an
+individual license agreement.
 -----------------------------------------------------------------------------
 */
 
